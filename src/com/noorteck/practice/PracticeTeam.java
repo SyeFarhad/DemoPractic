e2f3fcc4bb0fee40303dc69446a56736c68c69c6
sayedfarhad <sayedfarhad@sayeds-mbp-2.hsd1.ca.comcast.net>
2023-01-10 18:51:26 -0800

    This is my last practice

---
+++ src/com/noorteck/practice/PracticeTeam.java
@@ -0,0 +1,31 @@
+package com.noorteck.practice;
+
+public class PracticeTeam {
+
+public static void main (String[]args) {
+	
+
+	int S1 = 88;
+	int S2 = 68;
+	int S3 = 92;
+	int S4 = 77;
+	int S5 = 49;
+
+	int resultS1 = 88 + 68 + 92 + 77 + 49;
+	int resultS2 = 88 - 68 - 92 - 77 - 49;
+	int resultS3 = (88 + 68 + 92 + 77 + 49) / 5;
+
+	System.out.println(88 + " + " + 68 + " + " + 92 + " + " + 77 + " + " + 49 + " = " + resultS1);
+	System.out.println(88 + " - " + 68 + " - " + 92 + " - " + 77 + " - " + 49 + " - " + resultS2);
+	System.out.println(88 + " / " + 68 + " + " + 92 + " + " + 77 + " + " + 49 + " + " + resultS3);
+
+	
+	
+	
+	
+	
+	
+}
+	
+	
+}
